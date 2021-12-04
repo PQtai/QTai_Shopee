94b2307bfe9af00409a7a6b21d46c76995468d6c
PQtai <phamquoctai09082001@gmail.com>
2021-12-04 09:55:52 +0700

    fix day2

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,9 +42,56 @@ btnleft.onclick = function() {
     }
 };
 
-
-
+const navlogin = document.getElementById('login');
+const navregister = document.getElementById('resgister');
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+var modal = $('.modal');
+var modalLogin = $('.auth-form');
+var modalregister = $('.auth-form:last-child');
+var modaloverlay = $('.modal__overlay');
+var btnformAuths = $$('.auth-form__switch-btn');
+var auths = $$('.auth-form')
+var btnbackAuths = $$('.form__controls-btn-back');
+function handleLogicModal() {
+    modal.style.display = 'none';
+    modalLogin.classList.remove('form--display');
+    modalregister.classList.remove('form--display');
+}
+function eventhandling(){
+    // handle event click login
+    navlogin.onclick = function(){
+        modal.style.display = 'flex';
+        modalLogin.classList.add('form--display');
+    }
+    // handle event click register
+    navregister.onclick = function(){
+        modal.style.display = 'flex';
+        modalregister.classList.add('form--display');
+    }
+    // handle event click modaloverlay
+    modaloverlay.onclick = function(){
+        handleLogicModal();
+    }
+    // handle event click changeauthen
+    btnformAuths.forEach((btnformAuth) => {
+        btnformAuth.onclick = function(){
+            if(modalLogin.classList.contains('form--display')){
+                modalLogin.classList.remove('form--display');
+                modalregister.classList.add('form--display');
+            }else {
+                modalregister.classList.remove('form--display');
+                modalLogin.classList.add('form--display');
+            }
+        };
+    });
+    btnbackAuths.forEach((btnbackAuth) => {
+        btnbackAuth.onclick = function(){
+            handleLogicModal();
+        }
+    })
+}
 function start() {
-
+    eventhandling();
 }
 start();
